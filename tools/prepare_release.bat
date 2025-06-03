02593a03b1de13366f29e89f7bf611edef4de90a
Cem GEÇGEL <gecgelcem@outlook.com>
2025-06-03 14:08:58 +0300

    feat: Add `prepare_release` Tool

---
+++ tools/prepare_release.bat
@@ -0,0 +1,31 @@
+@echo off
+
+setlocal enabledelayedexpansion
+
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do (
+    for /f "tokens=1 delims=." %%b in ("%%a") do (
+        set timeStamp=%%b
+    )
+)
+
+set projectDirectory=%~dp0..
+set sourceDirectory=%projectDirectory%\hlml\src
+set assetDirectory=%projectDirectory%\assets
+set releaseDirectory=%projectDirectory%\releases
+
+rmdir /s /q "%releaseDirectory%\build"
+mkdir "%releaseDirectory%\build"
+for /r %sourceDirectory% %%i in (*) do (
+    set sourceFiles=!sourceFiles! %%i
+)
+javac -d "%releaseDirectory%\build" %sourceFiles%
+
+rmdir /s /q "%releaseDirectory%\latest"
+mkdir "%releaseDirectory%\latest"
+xcopy /v /e "%assetDirectory%" "%releaseDirectory%\latest"
+cd "%releaseDirectory%\build"
+jar cfe "%releaseDirectory%\latest\hlml.jar" hlml.launcher.Launcher *
+
+cd "%releaseDirectory%\latest"
+7z a -tzip "..\hlml-%timeStamp%.zip" "*"
+cd "..\.."
